83dfd1970b37ae88826dcb835522d870abb3baac
BuildTools <unconfigured@null.spigotmc.org>
2021-06-29 21:02:46 -0700

    Updated Lots of Stuffs

--- src/main/java/main/Events/GuildMessageEvent.java
+++ src/main/java/main/Events/GuildMessageEvent.java
@@ -1,7 +1,10 @@
 package main.Events;
 
 import main.Data.Data;
+import main.main;
 import main.utils.Utils;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -11,11 +14,56 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class GuildMessageEvent extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent e){
+
+
+        switch(e.getMessage().getContentRaw().toLowerCase().split(" ")[0]){
+            case "?help":{
+                e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                    put("Help Section","**?help**\nThis Page\n**?addMod \"example#1234\"**\nAdd a moderator\n" +
+                            "**?addAdmin \"example#1234\"**\nAdd an admin user");
+                }})).queue();
+                break;
+            }
+            case "?addmod":{
+                if(e.getMessage().getContentRaw().split(" ").length==2){
+                    Guild guild = main.jda.getGuilds().get(0);
+                    Member member = null;
+                    try{
+                        member = guild.getMemberByTag(e.getMessage().getContentRaw().split(" ")[1]);
+
+                        if(member!=null){
+
+                            //*******************************
+                            //ADD USER TO DATA ARRAY AND CONFIG FILE
+                            e.getChannel().sendMessage("worked").queue();
+
+                            //********************************
+                        }else{
+                            e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                            }})).queue();
+                        }
+                    }catch (java.lang.IllegalArgumentException exception){
+                        e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                            put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                        }})).queue();
+                    }
+                }else{
+                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                        put("Moderation Bot","?addMod format: ?addMod username#1234 (case sensitive)");
+                    }})).queue();
+                }
+
+                break;
+            }
+        }
+
         try{
             for(String string : e.getMessage().getContentRaw().split(" ")){
                 if(Data.bannedWords.contains(string)){
@@ -26,6 +74,7 @@ public class GuildMessageEvent extends ListenerAdapter {
             System.out.println("\033[0;31m" + "ERROR: Please give me Admin Permissions so I can moderate properly");
             System.out.print("\033[0m");
         }
+
 
     }
 }
--- src/main/java/main/utils/Utils.java
+++ src/main/java/main/utils/Utils.java
@@ -1,9 +1,14 @@
 package main.utils;
 
 import main.main;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
+import java.awt.*;
 import java.io.File;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 public class Utils {
@@ -14,7 +19,7 @@ public class Utils {
 
     public static String getDirPath() throws URISyntaxException {
         if(getOperatingSystem().toLowerCase().contains("windows 10")){
-            System.out.println("\033[0;31m" + "ERROR: Starting on Operating System Mode: "+getOperatingSystem());
+            System.out.println("\033[0;31m" + "Starting on Operating System Mode: "+getOperatingSystem());
             System.out.print("\033[0m");
             String[] pathSections = getPath().split(Pattern.quote("\\"));
             String DirPath = new String();
@@ -50,4 +55,22 @@ public class Utils {
     public static String getOperatingSystem() {
         return System.getProperty("os.name");
     }
+
+    public static MessageEmbed createEmbed(HashMap<String,String> content){
+
+        Guild guild = main.jda.getGuilds().get(0);
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Moderation Bot");
+        embedBuilder.setDescription("This is in BETA - DM Yoursole1#7254 with issues");
+        embedBuilder.setThumbnail(guild.getIconUrl());
+
+        for(String field : content.keySet()){
+            embedBuilder.addField(field,content.get(field),false);
+        }
+
+        embedBuilder.setFooter("Created with \u2665 by Yoursole1#7254");
+        embedBuilder.setColor(new Color(2871056));
+        return embedBuilder.build();
+    }
+
 }
