08d3a62d65e73579f1950be6e7180a32364a2bb8
BuildTools <unconfigured@null.spigotmc.org>
2021-07-11 12:05:49 -0700

    Added ?removeMod and ?removeAdmin Also fixed some automatic config file formatting

--- src/main/java/main/Events/GuildMessageEvent.java
+++ src/main/java/main/Events/GuildMessageEvent.java
@@ -46,9 +46,11 @@ public class GuildMessageEvent extends ListenerAdapter {
                                 }else{
                                     newText = oldText + "" + tag;
                                 }
+                                newText = newText.replace(",,",",");
 
 
                                 try {
+                                    main.moderatorText=newText;
                                     Scanner sc = new Scanner(new File(Utils.getDirPathNoLog()+"/Config.txt"));
                                     StringBuffer buffer = new StringBuffer();
                                     while (sc.hasNextLine()) {
@@ -110,7 +112,256 @@ public class GuildMessageEvent extends ListenerAdapter {
 
                 break;
             }
+            case "?addadmin":{
+                if(e.getMessage().getContentRaw().split(" ").length==2){
+                    Guild guild = main.jda.getGuilds().get(0);
+                    Member member = null;
+                    try{
+                        member = guild.getMemberByTag(e.getMessage().getContentRaw().split(" ")[1]);
+
+                        if(member!=null){
+                            if(!Data.admins.contains(member)){
+                                //*******************************
+                                //ADD USER TO DATA ARRAY AND CONFIG FILE
+                                String tag = e.getMessage().getContentRaw().split(" ")[1];
+                                String oldText = main.adminText;
+                                String newText = "";
+                                if(oldText.split(":").length>1&&oldText.split(":")[1].length()>4){
+                                    newText = oldText + "," + tag;
+                                }else{
+                                    newText = oldText + "" + tag;
+                                }
+
+                                newText = newText.replace(",,",",");
+
+                                try {
+                                    main.adminText=newText;
+                                    Scanner sc = new Scanner(new File(Utils.getDirPathNoLog()+"/Config.txt"));
+                                    StringBuffer buffer = new StringBuffer();
+                                    while (sc.hasNextLine()) {
+                                        buffer.append(sc.nextLine()+System.lineSeparator());
+                                    }
+                                    String fileContents = buffer.toString();
+                                    sc.close();
+                                    fileContents = fileContents.replaceAll(oldText, newText);
+                                    FileWriter writer = new FileWriter(Utils.getDirPathNoLog()+"/Config.txt");
+                                    writer.append(fileContents);
+                                    writer.flush();
+
+                                    Data.admins.add(member);
+
+                                    Member finalMember = member;
+
+                                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                        put("Moderation Bot", finalMember.getAsMention() + " is now an admin!");
+                                    }})).queue();
+
+                                } catch (FileNotFoundException | URISyntaxException fileNotFoundException) {
+
+
+                                    System.out.println("\033[0;31m" + "ERROR: You had the config file, but deleted it...why?????");
+                                    System.out.print("\033[0m");
+                                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                        put("Moderation Bot","You broke something, please check the bot's console");
+                                    }})).queue();
+                                } catch (IOException exception) {
+                                    System.out.println("\033[0;31m" + "ERROR: You had the config file, but deleted it...why?????");
+                                    System.out.print("\033[0m");
+                                    System.out.println("Please note that if you did not delete the config file this is the dev's fault, please contact us");
+
+                                }
+
+
+                                //********************************
+                            }else{
+                                e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                    put("Moderation Bot","This user is already an admin!");
+                                }})).queue();
+                            }
+
+                        }else{
+                            e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                            }})).queue();
+                        }
+                    }catch (java.lang.IllegalArgumentException exception){
+                        e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                            put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                        }})).queue();
+                    }
+                }else{
+                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                        put("Moderation Bot","?addAdmin format: ?addAdmin username#1234 (case sensitive)");
+                    }})).queue();
+                }
+
+                break;
+            }
+            case "?removemod":{
+                if(e.getMessage().getContentRaw().split(" ").length==2){
+                    Guild guild = main.jda.getGuilds().get(0);
+                    Member member = null;
+                    try{
+                        member = guild.getMemberByTag(e.getMessage().getContentRaw().split(" ")[1]);
+
+                        if(member!=null){
+                            if(Data.moderators.contains(member)){
+                                //*******************************
+                                //ADD USER TO DATA ARRAY AND CONFIG FILE
+                                String tag = e.getMessage().getContentRaw().split(" ")[1];
+                                String oldText = main.moderatorText;
+                                String newText = oldText.replace(tag,"").replace(",,",",");
+                                if(newText.split(",").length==1){
+                                    newText=newText.replace(",","");
+                                }
+
+
+                                try {
+                                    main.moderatorText=newText;
+                                    Scanner sc = new Scanner(new File(Utils.getDirPathNoLog()+"/Config.txt"));
+                                    StringBuffer buffer = new StringBuffer();
+                                    while (sc.hasNextLine()) {
+                                        buffer.append(sc.nextLine()+System.lineSeparator());
+                                    }
+                                    String fileContents = buffer.toString();
+                                    sc.close();
+                                    fileContents = fileContents.replaceAll(oldText, newText);
+                                    FileWriter writer = new FileWriter(Utils.getDirPathNoLog()+"/Config.txt");
+                                    writer.append(fileContents);
+                                    writer.flush();
+
+                                    Data.moderators.remove(member);
+
+                                    Member finalMember = member;
+
+                                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                        put("Moderation Bot", finalMember.getAsMention() + " is no longer a moderator D:");
+                                    }})).queue();
+
+                                } catch (FileNotFoundException | URISyntaxException fileNotFoundException) {
+
+
+                                    System.out.println("\033[0;31m" + "ERROR: You had the config file, but deleted it...why?????");
+                                    System.out.print("\033[0m");
+                                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                        put("Moderation Bot","You broke something, please check the bot's console");
+                                    }})).queue();
+                                } catch (IOException exception) {
+                                    System.out.println("\033[0;31m" + "ERROR: You had the config file, but deleted it...why?????");
+                                    System.out.print("\033[0m");
+                                    System.out.println("Please note that if you did not delete the config file this is the dev's fault, please contact us");
+
+                                }
+
+
+                                //********************************
+                            }else{
+                                e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                    put("Moderation Bot","This user is not a moderator!");
+                                }})).queue();
+                            }
+
+                        }else{
+                            e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                            }})).queue();
+                        }
+                    }catch (java.lang.IllegalArgumentException exception){
+                        e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                            put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                        }})).queue();
+                    }
+                }else{
+                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                        put("Moderation Bot","?removeMod format: ?removeMod username#1234 (case sensitive)");
+                    }})).queue();
+                }
+
+                break;
+            }
+            case "?removeadmin":{
+                if(e.getMessage().getContentRaw().split(" ").length==2){
+                    Guild guild = main.jda.getGuilds().get(0);
+                    Member member = null;
+                    try{
+                        member = guild.getMemberByTag(e.getMessage().getContentRaw().split(" ")[1]);
+
+                        if(member!=null){
+                            if(Data.admins.contains(member)){
+                                //*******************************
+                                //ADD USER TO DATA ARRAY AND CONFIG FILE
+                                String tag = e.getMessage().getContentRaw().split(" ")[1];
+                                String oldText = main.adminText;
+                                String newText = oldText.replace(tag,"").replace(",,",",");
+                                if(newText.split(",").length==1){
+                                    newText=newText.replace(",","");
+                                }
+
+
+                                try {
+                                    main.adminText=newText;
+                                    Scanner sc = new Scanner(new File(Utils.getDirPathNoLog()+"/Config.txt"));
+                                    StringBuffer buffer = new StringBuffer();
+                                    while (sc.hasNextLine()) {
+                                        buffer.append(sc.nextLine()+System.lineSeparator());
+                                    }
+                                    String fileContents = buffer.toString();
+                                    sc.close();
+                                    fileContents = fileContents.replaceAll(oldText, newText);
+                                    FileWriter writer = new FileWriter(Utils.getDirPathNoLog()+"/Config.txt");
+                                    writer.append(fileContents);
+                                    writer.flush();
+
+                                    Data.admins.remove(member);
+
+                                    Member finalMember = member;
+
+                                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                        put("Moderation Bot", finalMember.getAsMention() + " is no longer an admin D:");
+                                    }})).queue();
+
+                                } catch (FileNotFoundException | URISyntaxException fileNotFoundException) {
+
+
+                                    System.out.println("\033[0;31m" + "ERROR: You had the config file, but deleted it...why?????");
+                                    System.out.print("\033[0m");
+                                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                        put("Moderation Bot","You broke something, please check the bot's console");
+                                    }})).queue();
+                                } catch (IOException exception) {
+                                    System.out.println("\033[0;31m" + "ERROR: You had the config file, but deleted it...why?????");
+                                    System.out.print("\033[0m");
+                                    System.out.println("Please note that if you did not delete the config file this is the dev's fault, please contact us");
+
+                                }
+
+
+                                //********************************
+                            }else{
+                                e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                    put("Moderation Bot","This user is not an admin!");
+                                }})).queue();
+                            }
+
+                        }else{
+                            e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                                put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                            }})).queue();
+                        }
+                    }catch (java.lang.IllegalArgumentException exception){
+                        e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                            put("Moderation Bot","Please specify a real user (Case Sensitive)");
+                        }})).queue();
+                    }
+                }else{
+                    e.getChannel().sendMessage(Utils.createEmbed(new HashMap<>(){{
+                        put("Moderation Bot","?removeAdmin format: ?removeAdmin username#1234 (case sensitive)");
+                    }})).queue();
+                }
+                break;
+            }
         }
+
 
         try{
             for(String string : e.getMessage().getContentRaw().split(" ")){
@@ -122,7 +373,5 @@ public class GuildMessageEvent extends ListenerAdapter {
             System.out.println("\033[0;31m" + "ERROR: Please give me Admin Permissions so I can moderate properly");
             System.out.print("\033[0m");
         }
-
-
     }
 }
